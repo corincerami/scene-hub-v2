04bdcd80b8ad2a0029d5a18c6e5b7931b8eddfbd
Vikram Ramakrishnan <lord.ezar@gmail.com>
2014-12-24 11:30:02 -0600

    comments for chris cerami breakable toy

--- app/models/band.rb
+++ app/models/band.rb
@@ -2,7 +2,16 @@ class Band < ActiveRecord::Base
   has_many :gigs
   has_many :shows, through: :gigs
 
-  validates :name, presence: true
-
   belongs_to :user
+  # what is the benefit of a band belonging to a user?
+  # is a user here forced to be a band or can be anyone
+  # posting a band / show? Or maybe you want to know
+  # who posted the band? Right now, as a user, I can post
+  # multiple bands. I don't know if that was your intent
+  # but if it isn't, you'd want to put some limits into
+  # place
+
+  validates :name, presence: true
+  # ^^ just reordered here to separate associations from
+  # validations
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,3 +8,7 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
 end
+
+# Why do you have a test folder and a spec folder? Was this
+# created when you first made the app? If all your tests
+# are in spec, you won't need this folder
