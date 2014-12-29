df9824f4b83a74bd8f423b563928af9dfe639d0d
Chris C Cerami <chrisccerami@gmail.com>
2014-12-28 23:32:26 -0500

    Added authentications so that only the user who creates a band can post status updates to that band's profile.

--- app/controllers/band_posts_controller.rb
+++ app/controllers/band_posts_controller.rb
@@ -7,6 +7,10 @@ class BandPostsController < ApplicationController
 	def create
 		@band = Band.find(params[:band_id])
 		@band_post = BandPost.new(band_posts_params)
+		if !correct_user?
+			flash[:error] = "You don't have permission to do that"
+			redirect_to band_path(@band) and return
+		end
 		if @band_post.save
 			flash[:notice] = "Status update posted!"
 			redirect_to band_path(@band)
@@ -22,4 +26,9 @@ class BandPostsController < ApplicationController
 		band_post_params[:band_id] = params[:band_id]
 		band_post_params
 	end
+
+	def correct_user?
+    	@band = Band.find(params[:band_id])
+    	current_user == @band.user
+  	end
 end
