3a4f7586ee888dee5e56b7c86c0a9b297c79e863
Chris C Cerami <chrisccerami@gmail.com>
2014-12-29 00:14:36 -0500

    Added test for editing a band status update with an invalid form submission

--- spec/features/user_edits_a_band_status_update_spec.rb
+++ spec/features/user_edits_a_band_status_update_spec.rb
@@ -21,4 +21,24 @@ feature "User edits a band status update" do
 		expect(page).to have_content "New post content"
 		expect(page).to have_content "Status updated!"
 	end
+
+	it "leave the form blank" do
+		user = create(:user_with_bands)
+		band = user.bands.first
+		band.band_posts << create(:band_post)
+
+		visit new_user_session_path
+		fill_in "Email",    with: user.email
+		fill_in "Password", with: user.password
+		click_on "Log in"
+
+		visit band_path(band)
+		click_on "Edit status"
+		fill_in "Title",   with: ""
+		fill_in "Content", with: ""
+		click_on "Save changes"
+
+		expect(page).to have_content "Title can't be blank"
+		expect(page).to have_content "Content can't be blank"
+	end
 end
