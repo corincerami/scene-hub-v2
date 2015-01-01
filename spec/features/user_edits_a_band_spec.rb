4bd53b3a271829ccd0c0f6925433cbdab735a2b9
Chris <chrisccerami@gmail.com>
2015-01-01 15:25:37 -0500

    Added tests for editing an deleting a band

---
+++ spec/features/user_edits_a_band_spec.rb
@@ -0,0 +1,12 @@
+require "rails_helper"
+
+feature "User edits a band" do
+	it "submits a valid form" do
+	end
+
+	it "submits an invalid form" do
+	end
+
+	it "attempts to edit a band the user did not create" do
+	end
+end
