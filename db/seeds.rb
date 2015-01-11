6735b5d4bbaa06e05d294cf5dceeb24dcf92abaf
Chris C Cerami <chrisccerami@gmail.com>
2015-01-11 15:53:55 -0500

    Added seed data for development

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,49 +1,34 @@
-# include Geokit::Geocoders
+100.times do
+  user = User.new(email: Faker::Internet.email, password: Faker::Internet.password)
+  user.save
+  puts "user created"
+end
 
-# user_1 = User.create(email: "user_1@example.com", password: "password123")
-# user_2 = User.create(email: "another_1@user.com", password: "password123")
+User.all.each do |user|
+  band = user.bands.build(name: Faker::App.name)
+  band.save
+  puts "band created"
+end
 
-# band_1 = Band.create(name: "Screaming Females", user_id: 1)
-# band_2 = Band.create(name: "The Julie Ruin",    user_id: 2)
+100.times do
+  venue = Venue.new(name: Faker::Company.name,
+                    street_1: Faker::Address.street_address,
+                    city: Faker::Address.city,
+                    state: "MA",
+                    zip_code: Faker::Address.zip_code)
+  venue.save
+  puts "venue created"
+end
 
-# address_1 = "1222 Commonwealth Ave, Allston, MA"
-# loc_1 = MultiGeocoder.geocode(address_1)
+Venue.all.each do |venue|
+  show = venue.shows.build(details: Faker::Lorem.sentence,
+                           show_date: DateTime.now + 1.year)
+  show.save
+  puts "show created"
+end
 
-# venue_1 = Venue.create(
-# 	name:     "Great Scott",
-# 	street_1: "1222 Commonwealth Ave",
-# 	city:     "Allston",
-# 	state:    "MA",
-# 	zip_code: "02134",
-# 	lat:      loc_1.lat,
-# 	lng:      loc_1.lng )
-
-# address_2 = "149 5th Ave, New York, NY"
-# loc_2 = MultiGeocoder.geocode(address_2)
-
-# venue_2 = Venue.create(
-# 	name:     "Terminal 5",
-# 	street_1: "149 5th Ave",
-# 	city:     "New York",
-# 	state:    "NY",
-# 	zip_code: "10019",
-# 	lat:      loc_2.lat,
-# 	lng:      loc_2.lng )
-
-# show_1 = Show.create(
-# 	details:  "Some show",
-# 	show_date: DateTime.now,
-# 	venue_id: venue_1.id)
-
-# show_2 = Show.create(
-# 	details:  "Another show",
-# 	show_date: DateTime.now,
-# 	venue_id: venue_2.id)
-
-# gig_1 = Gig.create(
-# 	show_id:  show_1.id,
-# 	band_id:  band_1.id)
-
-# gig_2 = Gig.create(
-# 	show_id:  show_2.id,
-# 	band_id:  band_2.id)
+Show.all.each do |show|
+  gig = show.gigs.build(band: Band.find(rand(1..99)))
+  gig.save
+  puts "gig created"
+end
