52573ab6dac58d9d55e0d94ed0462728bc160885
Chris C Cerami <chrisccerami@gmail.com>
2015-01-13 08:37:09 -0500

    Shows index is now ordered whether or not a search is performed

--- app/models/show.rb
+++ app/models/show.rb
@@ -17,7 +17,7 @@ class Show < ActiveRecord::Base
 
   def self.search(zip_code, radius, genre)
     if !zip_code.nil? && !zip_code.empty?
-      shows = self.joins(:venue).joins(:bands).within(radius.to_i, origin: zip_code).where("show_date > ?", DateTime.now)
+      shows = self.joins(:venue).joins(:bands).within(radius.to_i, origin: zip_code).order(:show_date).where("show_date > ?", DateTime.now)
       if !genre.empty? && !genre.nil?
         # only return shows whose bands match the supplied genre
         shows = shows.order(:show_date).to_a.select { |show| show.bands.first.has_genre?(genre) }
@@ -26,7 +26,7 @@ class Show < ActiveRecord::Base
         shows
       end
     else
-      where("show_date > ?", DateTime.now)
+      order(:show_date).where("show_date > ?", DateTime.now)
     end
   end
 end
