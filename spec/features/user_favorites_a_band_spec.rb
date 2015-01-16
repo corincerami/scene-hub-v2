b777c5a93cedf66e16692476bcf44ff72b3835ea
Chris C Cerami <chrisccerami@gmail.com>
2015-01-16 14:33:48 -0500

    Followed bands now appear on user profile

--- spec/features/user_favorites_a_band_spec.rb
+++ spec/features/user_favorites_a_band_spec.rb
@@ -9,34 +9,35 @@ require "rails_helper"
 # - [ ] My followed bands should appear on my profile
 
 feature "User favorites a band" do
+  before(:each) do
+    @band = create(:band)
+    @user = create(:user)
+    sign_in(@user)
+
+    visit band_path(@band)
+
+    click_button "Follow #{@band.name}"
+  end
+
   it "adds a band to favorites" do
-    band = create(:band)
-    user = create(:user)
-    sign_in(user)
-
-    visit band_path(band)
-
-    click_button "Follow #{band.name}"
-
-    expect(page).to have_content "Now following #{band.name}"
+    expect(page).to have_content "Now following #{@band.name}"
     expect(page).to have_content "Following"
-    expect(page).to have_button "Stop following #{band.name}"
-    expect(page).not_to have_button "Follow #{band.name}"
+    expect(page).to have_button "Stop following #{@band.name}"
+    expect(page).not_to have_button "Follow #{@band.name}"
   end
 
   it "removes a band from favorites" do
-    band = create(:band)
-    user = create(:user)
-    sign_in(user)
+    click_button "Stop following #{@band.name}"
 
-    visit band_path(band)
-
-    click_button "Follow #{band.name}"
-    click_button "Stop following #{band.name}"
-
-    expect(page).to have_content "No longer following #{band.name}"
+    expect(page).to have_content "No longer following #{@band.name}"
     expect(page).not_to have_content "Following"
-    expect(page).to have_button "Follow #{band.name}"
-    expect(page).not_to have_button "Stop following #{band.name}"
+    expect(page).to have_button "Follow #{@band.name}"
+    expect(page).not_to have_button "Stop following #{@band.name}"
+  end
+
+  it "sees followed bands on the user profile" do
+    visit user_path(@user)
+
+    expect(page).to have_content "#{@band.name}"
   end
 end
